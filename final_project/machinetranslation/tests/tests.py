6970e094fae9335a40d3baa08b1025d17243b5fa
kmohil <kmohil54@gmail.com>
2023-07-29 14:31:34 -0400

    Final changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,13 @@
+import unittest
+from translator import english_to_french, french_to_english
+class TestTranslator(unittest.TestCase):
+    def test_e2f(self):
+        self.assertEqual(english_to_french('hello'), 'bonjour')
+        self.assertNotEqual(english_to_french('name'), 'bonjour')
+
+    def test_f2e(self):
+        self.assertEqual(french_to_english('bonjour'), 'hello')
+        self.assertNotEqual(french_to_english('monsieur'), 'name')
+
+if __name__ == '__main__':
+    unittest.main()
